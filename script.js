3e703c95659e697a244495d450dece9bf752dcf0
karl-no <karl.publicidade@gmail.com>
2022-07-13 20:59:45 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -24,19 +24,19 @@ const createProductItemElement = ({ sku, name, image }) => {
   return section;
 };
 
-// const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
-// const cartItemClickListener = (event) => {
-//   // coloque seu código aqui
-// };
+const cartItemClickListener = (event) => {
+  // coloque seu código aqui
+};
 
-// const createCartItemElement = ({ sku, name, salePrice }) => {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// };
+const createCartItemElement = ({ sku, name, salePrice }) => {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+};
 
 const createProductsList = async () => {
   const sectionProducts = document.querySelector('.items');
@@ -46,6 +46,21 @@ const createProductsList = async () => {
   });
 };
 
-createProductsList();
+const addToCart = async (item) => {
+  const cartList = document.querySelector('.cart__items');
+  const itemSku = getSkuFromProductItem(item.target.parentElement);
+  const { id: sku, title: name, price: salePrice } = await fetchItem(itemSku);
+  cartList.appendChild(createCartItemElement({ sku, name, salePrice }));
+};
+
+const itemListener = async () => {
+  await createProductsList();
+  const buttonAddItem = document.querySelectorAll('.item__add');
+  buttonAddItem.forEach((button) => {
+    button.addEventListener('click', addToCart);
+  });
+};
+
+itemListener();
 
 window.onload = () => { };
