96ddb355743ff48afd84d0012cd8cc9e2abc7427
karl-no <karl.publicidade@gmail.com>
2022-07-13 21:07:30 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -27,7 +27,8 @@ const createProductItemElement = ({ sku, name, image }) => {
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {
-  // coloque seu código aqui
+  const removeCart = event.target.parentNode;
+  removeCart.removeChild(event.target);
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
