dd635dd073a8323e84b3858bf21f746e8b8436aa
karl-no <karl.publicidade@gmail.com>
2022-07-14 18:33:46 -0300

    Requisito 6

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,5 +1,8 @@
-const saveCartItems = () => {
-  // seu código aqui
+const saveCartItems = (arg) => {
+  if (arg === undefined) {
+    return new Error('Nenhum item foi adicionado.');
+  }
+  localStorage.setItem('cartItems', arg);
 };
 
 if (typeof module !== 'undefined') {
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,16 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+  });
+  it('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com o parâmetro \'cartItems\' e o valor passado como argumento para saveCartItems', () => {
+    const param = '<ol><li>Item</li></ol>';
+    saveCartItems(param); 
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', param);
+  });
+  it('Testa se, caso não seja passado um argumento, a função saveCartItems retorn a mensagem de erro \'Nenhum item foi adicionado.\'', () => {
+    expect(saveCartItems()).toEqual(new Error('Nenhum item foi adicionado.'));
+  });
 });
